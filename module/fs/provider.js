4050d6db29e6767d6d85cdaf81eb1d65e4ff877e
Adrian Dmitra <adrian@dmitra.com>
2015-06-11 18:48:56 +0300

    consider dot in folder names

--- module/fs/provider.js
+++ module/fs/provider.js
@@ -31,14 +31,12 @@ Self.prototype.read = function () {
   self.tagPaths = {}
   self.items = {}
   self.duplicates = {}
-  self.pathItems = {}
   self.tagIds = {}
 
   files.forEach(self.findTagDuplicates.bind(self))
   files.forEach(self.parse.bind(self))
   //console.log(self.duplicates);
   //console.log(_.keys(self.duplicates).sort())
-  //console.log(pathItems);
   console.log(self.storage._items);
   console.log(self.storage._links);
   return self.storage
@@ -47,9 +45,7 @@ Self.prototype.read = function () {
 Self.prototype.findTagDuplicates = function (path) {
   var self = this
   var relative = Path.relative(self.inDir, path)
-  var names = relative.split(Path.sep)
-  var last = names[names.length -1]
-  names[names.length -1] = last.split('.')[0]
+  var names = self._getTagsFromPath(path)
 
   var last = _.last(names)
   if (self.tagPaths[last]) {
@@ -62,15 +58,12 @@ Self.prototype.findTagDuplicates = function (path) {
 
 Self.prototype.parse = function (path) {
   var self = this
-  var relative = Path.relative(self.inDir, path)
-  var names = relative.split(Path.sep)
-  var last = names[names.length -1]
-  //remove extension
-  names[names.length -1] = last.split('.')[0]
+  var names = self._getTagsFromPath(path)
   var items = []
 
-  names.forEach(function (name) {
-    if (_.contains(_.keys(self.duplicates), name)) {
+  names.forEach(function (name, index) {
+    //skip folders in the root for composing Groups
+    if (index > 0 && _.contains(_.keys(self.duplicates), name)) {
       items[items.length -1] = [items[items.length -1], name]
     } else items.push(name)
   })
@@ -83,7 +76,6 @@ Self.prototype.parse = function (path) {
     }
   }
 
-  self.pathItems[relative] = items
   self.parseSequence(items)
 }
 //Link parent-child
@@ -119,6 +111,17 @@ Self.prototype.parseGroup = function (items) {
     groupedItemIds.push(self.tagIds[item])
   })
   return groupedItemIds
+}
+
+Self.prototype._getTagsFromPath = function (path) {
+  var self = this
+  var relative = Path.relative(self.inDir, path)
+  //ignore file extension
+  if (!fs.lstatSync(path).isDirectory()) {
+    relative = relative.replace(Path.extname(path), '')
+  }
+  var tags = relative.split(/[\\\/\.]+/)
+  return tags
 }
 
 module.exports = Self
